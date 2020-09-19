93c238b00c2199088e4ba8c631b8cacb6d8e86c3
angithwin <atgtthwin.com>
2020-09-20 00:03:13 +0630

    First Commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+	require_once('db.php');
+	
+	//$db = new DB();
+	//$data = $db->viewData();
+	
+	//$data = $db->searchData('bu');
+	//$data = $db->viewData();
+	//var_dump($data);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width initial-scale=1.8">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	
+	<link rel="stylesheet" href="css/style.css"></link>
+	
+	<title>Content Search</title>
+</head>
+
+<body>
+
+<h1>Content Search</h1>
+
+<form action="search.php" method="POST">
+	<input type="text" name="name" placeholder="Search Here ..." 
+	id="searchBox" oninput=search(this.value)>
+</form>
+
+<script src="js/main.js"></script>
+
+
+</body>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,28 @@
+function search(name){
+	console.log(name);
+	fetchSearchData(name);
+}
+
+function fetchSearchData(name) {
+	fetch('search.php',{
+		method: "POST",
+		body: new URLSearchParams('name=' + name)
+	})
+	//.then(res => res.text())
+	.then(res => res.json())
+	.then(res => viewSearchResult(res))
+	//.then(res => console.log(res))
+	.catch(e => console.error('Error: ' + e))
+}
+
+function viewSearchResult(data){
+	const dataViewer = document.getElementById("dataViewer");
+	
+	dataViewer.innerHTML= "";
+	
+	for(let i=0; i < data.length; i++){
+		const li = document.createElement("li");
+		li.innerHTML = data[i]["quote"]
+		dataViewer.appendChild(li);
+	}
+}
